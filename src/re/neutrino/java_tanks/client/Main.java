d2d4f71d7283666754f71dad7ed01cd668a89658
Aleksander Mistewicz <a.mistewicz@gmail.com>
2016-01-03 14:51:49 +0100

    client: Add MainMenuPanel and GUI classes
    GUI extends JFrame. It will manage what JPanel is shown.
    MainMenuPanel extends JPanel. It's initial Panel displayed.

---
+++ src/re/neutrino/java_tanks/client/Main.java
@@ -0,0 +1,13 @@
+package re.neutrino.java_tanks.client;
+
+public class Main {
+	public static void main(String[] args) {
+		parseCommandLine(args);
+		GUI GUIframe = new GUI();
+	}
+	
+	private static void parseCommandLine(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+}
