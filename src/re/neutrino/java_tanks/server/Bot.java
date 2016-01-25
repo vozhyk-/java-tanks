6cda19afff4d18bfe0821c0bb434a73653b7aa6c
Aleksander Mistewicz <a.mistewicz@gmail.com>
2016-01-25 22:13:40 +0100

    bots: Now bot's at least try to hit a player

--- src/re/neutrino/java_tanks/server/Bot.java
+++ src/re/neutrino/java_tanks/server/Bot.java
@@ -11,7 +11,7 @@ public class Bot extends Client {
 	}
 
 	public void shoot() {
-		short power = 40, angle;
+		short power, angle;
 
 		/* choose target */
 		Player target = game.clients.get(0).getPlayer();
@@ -26,11 +26,57 @@ public class Bot extends Client {
 
 		short target_x = target.getPos().getX();
 		short my_x = getPlayer().getPos().getX();
+		power = (short) Math.abs(target_x-my_x);
+		if (power > 100) power = 100;
+		if (power < 30) power = 30;
+		int min = 8, impact_x, diff;
 
 		if (my_x > target_x) {
 			angle = 120;
+			while (true) {
+				impact_x = game.map.getImpact(this.getPlayer(), new Shot(angle, power)).getPos().getX();
+				diff = target_x - impact_x;
+				if (diff > 0) {
+					if (diff > min) {
+						angle-=5;
+					} else {
+						break;
+					}
+					if (angle < 110)
+						break;
+				} else {
+					if (diff < -min) {
+						angle+=5;
+					} else {
+						break;
+					}
+					if (angle > 130)
+						break;
+				}
+			}
 		} else {
 			angle = 60;
+			while (true) {
+				impact_x = game.map.getImpact(this.getPlayer(), new Shot(angle, power)).getPos().getX();
+				diff = impact_x - target_x;
+				if (diff > 0) {
+					if (diff > min) {
+						angle-=5;
+					} else {
+						break;
+					}
+					if (angle < 50)
+						break;
+				} else {
+					if (diff < -min) {
+						angle+=5;
+					} else {
+						break;
+					}
+					if (angle > 80)
+						break;
+				}
+			}
 		}
 
 		Shot shot = new Shot(angle, power);
